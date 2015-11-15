b3616846ef3c88f725b0f7a83501aa38a3c091fc
roseak <rose.kohn@gmail.com>
2015-11-14 21:38:58 -0700

    Added music player and gave it style to the playlist page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    @episodes = episodes(params[:search])
+    
+  end
+
+  private
+
+  def episodes(query)
+    OauthAudiosearch.new.search({q: query}, 'episodes')
   end
 end
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,5 +1,8 @@
 class ShowsController < ApplicationController
   def show
     @show = OauthAudiosearch.new.show(params[:id])
+    @related = OauthAudiosearch.new.related(params[:id], {type: "shows",
+                                                          size: 6,
+                                                          from: 6})
   end
 end
