41e677cfbe4c62b0b1b8015138f1a755375dec06
roseak <rose.kohn@gmail.com>
2015-11-16 12:55:43 -0700

    Added omniauth stub details.

--- test/integration/user_logs_in_with_twitter_test.rb
+++ test/integration/user_logs_in_with_twitter_test.rb
@@ -5,10 +5,34 @@ class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
 
   def setup
     Capybara.app = Podlist::Application
+    stub_omniauth
   end
 
   test "logging in" do
     visit "/"
     assert_equal 200, page.status_code
+
+    click_link "LOGIN"
+    assert_equal "/home", current_path
+    assert page.has_content?("Rosie")
+    assert page.has_link?("Logout")
+  end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      provider: "twitter",
+      extra: {
+        raw_info: {
+          user_id: "1234",
+          name: "Rosie",
+          screen_name: "kohniva",
+        }
+      },
+      credentials: {
+        token: "pizza",
+        secret: "secretpizza"
+      }
+    })
   end
 end
