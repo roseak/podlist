143dfe2b365a6b9eb3a69acbc70f05a8e7ef0847
roseak <rose.kohn@gmail.com>
2015-11-18 22:15:07 -0700

    Added dalli and memcachier.

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Podlist
   class Application < Rails::Application
-    config.cache_store = :redis_store, "redis://localhost:6379", {expires_in: 1.week}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
