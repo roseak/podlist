aa89f2aeb16bf182a0d5971e29986ddf283cf86a
roseak <rose.kohn@gmail.com>
2015-11-19 01:33:34 -0700

    Fixed related shows image for when an image doesn't exist.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -156,7 +156,13 @@ function fetchEpisodes(query){
   }
 
   function attachModal(){
-    $('.modal-trigger').leanModal()
+    $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .1, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+      }
+    );
   }
 
 }
